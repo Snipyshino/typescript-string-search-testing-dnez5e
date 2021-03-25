ca51fad7b1f56cb856fe325d33a2100bb613bc50
Snipyshino <u1653603@unimail.hud.ac.uk>
2021-03-25 16:08:55 +0000

    done

--- index.ts
+++ index.ts
@@ -1,22 +1,15 @@
-// Import stylesheets
-import './style.css';
-
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
 let base = 'eggs and ham and something else';
-let val = 'eggs';
+let vals = ['eggs', 'eggs(', '(eggs)', '[e-z]'];
 
 function escapeRegExp(string) {
-  return string.replace(/[{(]/g, '\\$&'); // $& means the whole matched string
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-const result = escapeRegExp(base).search(val) >= 0;
-const result2 = base.includes(val);
-
-appDiv.innerHTML =
-  `
-<h1>SSH</h1>
-<h2>Result: ${result}</h2>
-<h2>Result: ${result2}</h2>
-`;
+vals.forEach((val) => {
+console.log('search ' + (base.search(escapeRegExp(val)) >= 0));
+console.log('search unescaped ' + (base.search(val) >= 0));
+console.log('includes '+ base.includes(val));
+});
