c9034107383af84b8046b212811c0dd040887b3e
Snipyshino <u1653603@unimail.hud.ac.uk>
2021-03-25 17:36:15 +0000

    Made a working solution, but I hate it

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 
-let base = 'eggs and ham and something else';
-let vals = ['eggs', 'eggs(', '(eggs)', '[e-z]'];
+let base = "eggs and ham and something else";
+let vals = ["egg", "(eggs", "[e-z]", "eg[s"];
 
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
-
-vals.forEach((val) => {
-console.log('search ' + (base.search(escapeRegExp(val)) >= 0));
-console.log('search unescaped ' + (base.search(val) >= 0));
-console.log('includes '+ base.includes(val));
+vals.forEach(val => {
+  /**
+   * You can use a tryCatch to catch the syntax error and use includes instead
+   * In other words it can decide between using string or regex
+   */
+  try {
+    const reg = RegExp(val);
+    console.log("search " + val + " " + (base.search(reg) > -1));
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      console.log("includes " + val + " " + base.includes(val));
+    } else {
+      console.error(e);
+    }
+  }
 });
